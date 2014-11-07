e3395c688bf524c29e526d3b161dfeefe8b3fc0f
Thomas Leendertsen <thomas.leendertsen@lunatech.com>
2014-11-07 10:32:58 +0100

    add Creating_Classes

---
+++ Creating_Classes.scala
@@ -0,0 +1,30 @@
+class Hippo
+class Lion
+class Tiger
+class Monkey
+class Giraffe
+
+val a = new Hippo
+val b = new Lion
+val c = new Tiger
+val d = new Monkey
+val e = new Giraffe
+
+println(a + "" + b +  "" + c + "" +  d + "" + e )
+
+val f = new Lion
+val g = new Giraffe
+val h = new Giraffe
+
+println(f)
+println(g)
+println(h)
+
+//Answer ex 2 : They have different places in memory
+
+class Zebra{
+	println("got stripes")
+}
+
+val tunedHorse = new Zebra
+
