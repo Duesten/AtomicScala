35d380ffca839431b448fc9fb773b13df2d97abc
Thomas Leendertsen <thomas.leendertsen@lunatech.com>
2014-11-11 10:46:40 +0100

    add Family.scala

---
+++ Family.scala
@@ -0,0 +1,31 @@
+class Family(asd:String*){
+
+	def getFamilySize():Int = {
+		var familySize = 0
+		for(x <- asd){println(x) ; familySize = familySize + 1}
+		
+		familySize
+
+	}
+	
+}
+
+val a = new Family("a","b","c")
+
+println(a.getFamilySize)
+
+
+class FamilyExt(mom:String,dad:String,children:String*){
+
+	def getFamilySize:Int = {
+		var familySize = 0
+		for(x <- children){println(x) ; familySize = familySize + 1}
+		
+		{familySize + 2}
+
+	}
+	
+}
+
+val asd = new Family("Mom","Dad","Son","Daughter","Adopted kid")
+println(asd.getFamilySize)
--- PatternMatching.scala
+++ PatternMatching.scala
@@ -10,3 +10,20 @@ def matchColor(color:String):String = {
 
 println(matchColor("red"))
 
+val asd = Vector(100,80,50,20,0,15)
+
+def forecast(t: Int):String = {
+	t match {
+       case 100 => "Sunny"
+       case 80 => "Mostly Sunny"
+       case 50 => "Partly Sunny"
+       case 20 => "Mostly Cloudy"
+       case 0 => "Cloudy"
+       case _ => "Unknown"
+	}
+}
+
+for(x <- asd){
+	println(forecast(x))
+}
+
